1bbc12d0459bc0aaaf141d3ba44998700c108e2c
anagiovanni <anagiovanni66@gmail.com>
2024-07-25 12:28:26 -0300

    Criado o component de figura

---
+++ jogo-da-velha/components/figura.js
@@ -0,0 +1,13 @@
+import { TouchableOpacity } from 'react-native'
+import React from 'react'
+import {EnTypo } from '@expo/vector-icons'
+
+export default function figura() {
+  return (
+    <View>
+      <Text>figura</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
